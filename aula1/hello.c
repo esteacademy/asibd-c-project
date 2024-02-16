3a57109ba21fe169b4e8d33271365ef3add333a6
esteacademy <estefanio.academy@gmail.com>
2024-02-16 16:55:59 +0000

    first commit

---
+++ aula1/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>  // Preprocessador ou cabeçalho
+
+//int: significa o tipo de retorno da função
+//main: Nome da função, que no caso é a função prinicpla (Obrigatório)
+// (): Onde podemos passar os prarâmetros da função
+// {}, O coerpo da função, onde deve estar toda a lógica pretendida
+
+int main(){
+    printf("Olá mundo");
+}
+
